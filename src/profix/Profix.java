ad2c3a6d2d9b0d4271593cab6e2c10ed20be999b
SabeenaKumarawadu <sabeena1217@gmail.com>
2017-02-01 21:16:57 +0530

    initial commit

---
+++ src/profix/Profix.java
@@ -0,0 +1,13 @@
+package profix;
+
+import view.Login;
+
+public class Profix {
+
+    public static void main(String[] args) {
+        Login l = new Login();
+        l.setVisible(true);
+        l.setLocationRelativeTo(null);
+    }
+    
+}
